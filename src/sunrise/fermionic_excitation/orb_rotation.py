047f09923985b77ab4918d60788e8d0c555509a7
Javier <javier@Ordenador-portatil-de-Javier.local>
2025-04-29 17:12:34 +0200

    Work in progress

--- src/sunrise/fermionic_excitation/orb_rotation.py
+++ src/sunrise/fermionic_excitation/orb_rotation.py
@@ -10,14 +10,10 @@ class OrbitalRotation():
         assert len(self.orbital) == len(self.coeff)
         assert self.coeff.ndim == 2
     def __add__(self, other):
-        ov = [i for i in self.orbital if i in other.orbital]
-        if len(ov):
-            pass #how to carefully consider when overlap. i.e. [0,2] A + [0,4] B
-        else:
-            orb = self.orbital + other.orbital
-            t = pad(self.orbital,[(0,len(other.orbital)),(0,len(other.orbital))])
-            o = pad(other.orbital,[(len(self.orbital),0),(len(self.orbital),0)])
-            return OrbitalRotation(matrix=t+o,orbitals=orb)
+        orb = self.orbital + other.orbital
+        new_idx = [self.orbital]
+        t = pad(self.orbital,[(0,len(other.orbital)),(0,len(other.orbital))])
+        o = pad(other.orbital,[(len(self.orbital),0),(len(self.orbital),0)])
+        return OrbitalRotation(matrix=t+o,orbitals=orb)
     def compile(self)->QCircuit:
         pass
-    
--- src/sunrise/fermionic_excitation/work_in_progress.py
+++ src/sunrise/fermionic_excitation/work_in_progress.py
@@ -3,9 +3,27 @@ import sunrise as sun
 import numpy as np
 
 a = np.array([[0,1],[2,3]])
-print(a.reshape(()))
-idx = [1,3]
-n = np.array([[4,5],[6,7]])
-jdx = [0,2]
-new_shape = 4
-n = np.zeros(shape=(new_shape,new_shape))
+idx = [1,2]
+b = np.array([[0,1],[1,0]])
+jdx = [3,4]
+print('original a\n',a)
+print('original b\n', b)
+ndx = list(set(idx+jdx))
+print('ndx ',ndx)
+
+pos_idx = [ndx.index(i) for i in idx]
+pos_jdx = [ndx.index(i) for i in jdx]
+print('pos_idx ',pos_idx)
+print('pos_jdx ',pos_jdx)
+r_idx = [(idx[i],pos_idx[i]) for  i in range(len(idx))]
+r_jdx = [(jdx[i],pos_jdx[i]) for  i in range(len(jdx))]
+print("R idx ",r_idx)
+print("R jdx ",r_jdx)
+new_a = np.eye(len(ndx))
+new_b = np.eye(len(ndx))
+new_a[:len(a),:len(a)]=a
+new_b[:len(b),:len(b)]=b
+print('new_a\n',new_a)
+print('new_b\n',new_b)
+ra = np.zeros(shape=new_a.shape)
+rb = np.zeros(shape=new_b.shape)
