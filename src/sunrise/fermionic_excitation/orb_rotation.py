4c7b264184b8433dab7acf06ac9120b048749f86
Javier <javier@Ordenador-portatil-de-Javier.local>
2025-04-29 11:59:58 +0200

    Working on

--- src/sunrise/fermionic_excitation/orb_rotation.py
+++ src/sunrise/fermionic_excitation/orb_rotation.py
@@ -1,17 +1,23 @@
 import tequila as tq
 from tequila import QCircuit,QTensor
-from numpy import array,zeros
-tq.gates.QubitExcitation()
+from numpy import array,pad
 
-class FermionicExcitation():
+class OrbitalRotation():
 
-    def __init__(self,lfrom:[int]=None,lto:[int]=None,matrix:array=None):
-        self.orbital = lfrom + lto
+    def __init__(self,orbitals:[int]=None,matrix:array=None):
+        self.orbital = orbitals
         self.coeff = matrix #it will be changed to qtensor to allow variables
         assert len(self.orbital) == len(self.coeff)
         assert self.coeff.ndim == 2
     def __add__(self, other):
-        #it
-        pass
+        ov = [i for i in self.orbital if i in other.orbital]
+        if len(ov):
+            pass #how to carefully consider when overlap. i.e. [0,2] A + [0,4] B
+        else:
+            orb = self.orbital + other.orbital
+            t = pad(self.orbital,[(0,len(other.orbital)),(0,len(other.orbital))])
+            o = pad(other.orbital,[(len(self.orbital),0),(len(self.orbital),0)])
+            return OrbitalRotation(matrix=t+o,orbitals=orb)
     def compile(self)->QCircuit:
         pass
+    
