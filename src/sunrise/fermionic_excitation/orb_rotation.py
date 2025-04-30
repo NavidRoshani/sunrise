2c7a4678f0dc7d0f5435b3e787deab1d3d1ae0aa
JdelArco98 <javierdelarcosantos@gmail.com>
2025-04-30 16:18:34 +0200

    Orbital Rotation

--- src/sunrise/fermionic_excitation/orb_rotation.py
+++ src/sunrise/fermionic_excitation/orb_rotation.py
@@ -1,6 +1,6 @@
 import tequila as tq
 from tequila import QCircuit,QTensor
-from numpy import array,pad
+from numpy import array,zeros,eye,tensordot,allclose
 
 class OrbitalRotation():
 
@@ -9,11 +9,40 @@ class OrbitalRotation():
         self.coeff = matrix #it will be changed to qtensor to allow variables
         assert len(self.orbital) == len(self.coeff)
         assert self.coeff.ndim == 2
+        assert allclose(eye(len(self.coeff)), self.coeff.dot(self.coeff.T.conj()))
     def __add__(self, other):
-        orb = self.orbital + other.orbital
-        new_idx = [self.orbital]
-        t = pad(self.orbital,[(0,len(other.orbital)),(0,len(other.orbital))])
-        o = pad(other.orbital,[(len(self.orbital),0),(len(self.orbital),0)])
-        return OrbitalRotation(matrix=t+o,orbitals=orb)
-    def compile(self)->QCircuit:
+        ndx = list(set(self.orbital+other.orbital))
+        pos_idx = [ndx.index(i) for i in self.orbital]
+        pos_jdx = [ndx.index(i) for i in other.orbital]
+        new_a = eye(len(ndx))
+        new_b = eye(len(ndx))
+        new_a[:len(self.orbital), :len(self.orbital)] = self.coeff
+        new_b[:len(other.orbital), :len(other.orbital)] = other.coeff
+        ra = self._rot_mat(len(ndx), pos_idx)
+        rb = self._rot_mat(len(ndx), pos_jdx)
+        ap = ra.T.dot(new_a.dot(ra))
+        bp = rb.T.dot(new_b.dot(rb))
+        return OrbitalRotation(matrix=tensordot(ap,bp,axes=1),orbitals=ndx)
+    def compile(self,**kwargs)->QCircuit:
+        # TODO: fix line bellow properly
+        U = tq.quantumchemistry.qc_base.QuantumChemistryBase.get_givens_circuit(unitary=self.coeff,**kwargs)
+        U.map_qubits(qubit_map={i:idx for i,idx in enumerate(self.orbital)})
+        return U
+    def __eq__(self, other):
+        return self.orbital==other.orbital and allclose(self.coeff,other.coeff)
+    def _rot_mat(self,n: int, pos_dx):
+        m = zeros(shape=(n, n))
+        for i in range(len(pos_dx)):
+            m[i, pos_dx[i]] = 1
+        rest = [i for i in [*range(n)] if i not in pos_dx]
+        for i in range(len(rest)):
+            m[len(pos_dx) + i, rest[i]] = 1
+        return m
+    def __str__(self):
         pass
+        # result = "circuit: \n"
+        # for g in self.gates:
+        #     result += str(g) + "\n"
+        # return result
+    def __repr__(self):
+        return self.__str__()
--- src/sunrise/fermionic_excitation/work_in_progress.py
+++ src/sunrise/fermionic_excitation/work_in_progress.py
@@ -2,28 +2,40 @@ import tequila as tq
 import sunrise as sun
 import numpy as np
 
-a = np.array([[0,1],[2,3]])
-idx = [1,2]
-b = np.array([[0,1],[1,0]])
-jdx = [3,4]
-print('original a\n',a)
-print('original b\n', b)
-ndx = list(set(idx+jdx))
-print('ndx ',ndx)
-
-pos_idx = [ndx.index(i) for i in idx]
-pos_jdx = [ndx.index(i) for i in jdx]
-print('pos_idx ',pos_idx)
-print('pos_jdx ',pos_jdx)
-r_idx = [(idx[i],pos_idx[i]) for  i in range(len(idx))]
-r_jdx = [(jdx[i],pos_jdx[i]) for  i in range(len(jdx))]
-print("R idx ",r_idx)
-print("R jdx ",r_jdx)
-new_a = np.eye(len(ndx))
-new_b = np.eye(len(ndx))
-new_a[:len(a),:len(a)]=a
-new_b[:len(b),:len(b)]=b
-print('new_a\n',new_a)
-print('new_b\n',new_b)
-ra = np.zeros(shape=new_a.shape)
-rb = np.zeros(shape=new_b.shape)
+# def rot_mat(n:int,pos_dx):
+#     m = np.zeros(shape=(n,n))
+#     for i in range(len(pos_dx)):
+#         m[i,pos_dx[i]] = 1
+#     rest = [i for i in [*range(n)] if i not in pos_dx]
+#     print('REST ',rest)
+#     for i in range(len(rest)):
+#         m[len(pos_dx)+i,rest[i]] =1
+#     return m
+# a = np.array([[1,0],[1,0]])
+# idx = [1,2]
+# b = np.array([[0.1,0.2],[.3,0.4]])
+# jdx = [2,3]
+# print('original a\n',a)
+# print('original b\n', b)
+# ndx = list(set(idx+jdx))
+# print('ndx ',ndx)
+# pos_idx = [ndx.index(i) for i in idx]
+# pos_jdx = [ndx.index(i) for i in jdx]
+# print('pos_idx ',pos_idx)
+# print('pos_jdx ',pos_jdx)
+# new_a = np.eye(len(ndx))
+# new_b = np.eye(len(ndx))
+# new_a[:len(a),:len(a)]=a
+# new_b[:len(b),:len(b)]=b
+# # print('new_a\n',new_a)
+# # print('new_b\n',new_b)
+# ra = rot_mat(len(ndx),pos_idx)
+# rb = rot_mat(len(ndx),pos_jdx)
+# # print("rot_a:\n",ra)
+# # print("rot_b:\n",rb)
+# ap = ra.T.dot(new_a.dot(ra))
+# bp = rb.T.dot(new_b.dot(rb))
+# print("Final A\n",ap)
+# print("Final B\n",bp)
+# fin = np.tensordot(ap,bp,axes=1)
+# print("Result\n",fin)
