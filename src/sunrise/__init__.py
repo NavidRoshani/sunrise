3505c865b5f0df8db6daed9c8fa5cd492866d780
kottmanj <jakob.kottmann@gmail.com>
2025-07-31 14:25:16 +0200

    added proposition

--- src/sunrise/__init__.py
+++ src/sunrise/__init__.py
@@ -9,3 +9,40 @@ from sunrise.fermionic_excitation.orb_rotation import OrbitalRotation
 from sunrise.miscelaneus.giusepe import giuseppe
 from sunrise.miscelaneus.bar import giussepe_bar
 from sunrise.expval import Braket,show_available_modules,show_supported_modules
+
+import tequila as tq
+class FermionicCircuit:
+
+    def __init__(self, operations=None, initial_state=None):
+        self._operations = operations
+        if operations is None:
+            self._operations = []
+        self._initial_state = initial_state
+
+        self.verify()
+
+    def __add__(self, other):
+        initial_state = self._initial_state
+        operations = self._operations + other._operations
+        if hasattr(other, "_operations"):
+            if self._initial_state is None:
+                initial_state = other._initial_state
+            elif other._initial_state != self._initial_state:
+                raise Exception(f"FermionicCircuit + FermionicCircuit with two different initial states:\n{self._initial_state}, {other._initial_state}")
+
+        return FermionicCircuit(operations=operations, initial_state=initial_state)
+
+    def verify(self):
+        operations = [[tq.assign_variable(x[0]), tuple(x[1])] for x in self._operations]
+        self._operations = operations
+
+    def extract_variables(self):
+        pass
+
+    @property
+    def variables(self):
+        return self.extract_variables()
+
+def make_excitation_gate(indices, angle, *args, **kwargs):
+    return (angle, indices)
+
---
+++ testus.py
@@ -0,0 +1,4 @@
+import sunrise as sun
+
+ket = sun.FermionicCircuit(operations=[("a", [(0,2),(1,3)])], initial_state="|1100>")
+
