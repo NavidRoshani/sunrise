bbf206f4d1e65a336b5e51284236d649d1d62f05
JdelArco98 <javierdelarcosantos@gmail.com>
2025-08-12 11:06:34 +0200

    UX gate

--- src/sunrise/fermionic_excitation/gates.py
+++ src/sunrise/fermionic_excitation/gates.py
@@ -12,3 +12,7 @@ def UR(i:int,j:int, variables:typing.Union[typing.Hashable, numbers.Real, Variab
 
 def UC(i:int,j:int, variables:typing.Union[typing.Hashable, numbers.Real, Variable, FixedVariable]|None=None)->FCircuit:
     return FCircuit.wrap_gate(UCImpl(i,j,variables))
+
+def UX(indices:typing.Union[list,tuple]|None=None, variables:typing.Union[typing.Hashable, numbers.Real, Variable, FixedVariable]|None=None, reordered:bool=False)->FCircuit:
+    return FCircuit.wrap_gate(FermionicExcitationImpl(indices,variables,reordered))
+
